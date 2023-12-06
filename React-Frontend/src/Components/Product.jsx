4723d5f347917cf5563a292c6e18deb36db2bd5e
Subin <sub_1n@yahoo.com>
2023-12-06 22:03:12 +0200

    react prop types adjusted

---
+++ React-Frontend/src/Components/Product.jsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line react/prop-types
+const Product = ({ product }) => {
+  // eslint-disable-next-line react/prop-types
+  return <div>{`${product.f_name} ${product.l_name}`}</div>;
+};
+
+export default Product;
--- React-Frontend/src/pages/HomePage.jsx
+++ React-Frontend/src/pages/HomePage.jsx
@@ -1,10 +1,48 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Product from "../Components/Product";
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  return <div>This is home page</div>;
+  const getProducts = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get("http://localhost:3000/api/products/");
+      console.log(response.data);
+      setProducts(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <div className="mt-5">
+        {isLoading ? (
+          "Loading"
+        ) : (
+          <>
+            {products.length > 0 ? (
+              <>
+                {products.map((product, index) => {
+                  return <Product key={index} product={product} />;
+                })}
+              </>
+            ) : (
+              <div>There is no product</div>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
