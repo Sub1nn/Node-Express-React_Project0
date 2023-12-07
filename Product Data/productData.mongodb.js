98fe13e68136cf5307c658967fca2299dae05950
Subin <sub_1n@yahoo.com>
2023-12-07 11:50:59 +0200

    Product data added to mongoDB

---
+++ Product Data/productData.mongodb.js
@@ -0,0 +1,34 @@
+use("Node-API");
+
+// db.products.insertMany([
+//     {
+//         Name: "Macbook-Pro 14 M3",
+//         Company: "Apple",
+//         Price: 2500,
+//         Quantity: 15,
+//         Image: "https://www.gigantti.fi/product/tietokoneet-ja-toimistotarvikkeet/tietokoneet/kannettavat-tietokoneet/macbook-pro-16-m3-max-2023-361-tb-hopea/712415?context=yritysmyynti"
+//     },
+//     {
+//         Name: "MSI Katana 15 i7-13H",
+//         Company: "MSI",
+//         Price: 1700,
+//         Quantity: 25,
+//         Image: "https://www.gigantti.fi/product/gaming/pelitietokoneet/pelikannettavat/msi-katana-15-i7-13h1610004060-156-pelikannettava/650364?context=yritysmyynti"
+//     },
+//     {
+//         Name: "Acer Swift Edge 16 R7-7",
+//         Company: "Acer",
+//         Price: 1200,
+//         Quantity: 40,
+//         Image: "https://www.gigantti.fi/product/tietokoneet-ja-toimistotarvikkeet/tietokoneet/kannettavat-tietokoneet/acer-swift-edge-16-r7-7161024oled-kannettava/616164?context=yritysmyynti"
+//     },
+//     {
+//         Name: "Lenovo ThinkPad Z16 Gen1 R7",
+//         Company: "MSI",
+//         Price: 3100,
+//         Quantity: 20,
+//         Image: "https://www.gigantti.fi/product/tietokoneet-ja-toimistotarvikkeet/tietokoneet/kannettavat-tietokoneet/lenovo-thinkpad-z16-gen1-16-r716512-gb-kannettava/473769?context=yritysmyynti"
+//     }
+// ])
+
+db.products.find();
