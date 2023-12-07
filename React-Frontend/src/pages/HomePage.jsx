244e50f64c000af73d86f5d0fd22bcd67fb04623
Subin <sub_1n@yahoo.com>
2023-12-07 20:15:14 +0200

    navigation link designed to create a product

--- React-Frontend/src/pages/HomePage.jsx
+++ React-Frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Product from "../Components/Product";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
@@ -24,6 +25,9 @@ const HomePage = () => {
 
   return (
     <div>
+      <div className="inline-block mt-4 shadow-md bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer ">
+        <Link to="/create">Create a Product</Link>
+      </div>
       <div className="mt-5 grid grid-cols-2 lg:grid-cols-4 gap-4">
         {isLoading ? (
           "Loading"
