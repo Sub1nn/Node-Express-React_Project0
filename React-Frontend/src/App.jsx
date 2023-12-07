6d694ee1b27cbcd98c2be910cac6be3c7f4766aa
Subin <sub_1n@yahoo.com>
2023-12-07 17:23:08 +0200

    Nav-element adjusted

--- React-Frontend/src/App.jsx
+++ React-Frontend/src/App.jsx
@@ -6,18 +6,25 @@ import EditPage from "./pages/EditPage";
 const App = () => {
   return (
     <div>
-      <nav className="bg-gray-800">
-        <div className="container mx-auto p-2">
-          <Link to="/">
-            <h2 className="text-white text-2xl font-bold">React CRUD</h2>
-          </Link>
+      <nav className="bg-gray-800 flex justify-between items-center p-2">
+        <div className="container mx-auto">
+          <div className="flex justify-between ">
+            <Link to="/">
+              <h2 className="text-white text-2xl font-bold mr-4">
+                CRUD with React & Node
+              </h2>
+            </Link>
+            <h3 className="text-white font-bold pt-1.5 ">Created by: Subin</h3>
+          </div>
         </div>
       </nav>
-      <Routes>
-        <Route index element={<HomePage />}></Route>
-        <Route path="/create" element={<CreatePage />}></Route>
-        <Route path="/edit" element={<EditPage />}></Route>
-      </Routes>
+      <div className="container mx-auto h-full">
+        <Routes>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/create" element={<CreatePage />}></Route>
+          <Route path="/edit" element={<EditPage />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 };
