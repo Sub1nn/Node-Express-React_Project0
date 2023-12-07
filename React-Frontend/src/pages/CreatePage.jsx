f70591b974aad383468c53b47ea426a6fb0f9334
Subin <sub_1n@yahoo.com>
2023-12-07 20:13:52 +0200

    form added to create a product

--- React-Frontend/src/pages/CreatePage.jsx
+++ React-Frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,54 @@
 const CreatePage = () => {
   return (
-    <div>
-      <h1>In this page you can create a product</h1>
+    <div className="max-w-lg bg-white shadow-lg mx-auto p-5 rounded mt-6">
+      <h2 className="font-semibold text-2xl mb-4 block text-center ">
+        Create Product
+      </h2>
+      <form>
+        <div className="space-y-2">
+          <div>
+            <label>Name</label>
+            <input
+              type="text"
+              value=""
+              className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400"
+              placeholder="Enter Name"
+            />
+          </div>
+          <div>
+            <label>Quantity</label>
+            <input
+              type="number"
+              value=""
+              className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400"
+              placeholder="Enter Quantity"
+            />
+          </div>
+          <div>
+            <label>Price</label>
+            <input
+              type="number"
+              value=""
+              className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400"
+              placeholder="Enter Price"
+            />
+          </div>
+          <div>
+            <label>Image URL</label>
+            <input
+              type="text"
+              value=""
+              className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400"
+              placeholder="Enter Image URL"
+            />
+          </div>
+          <div>
+            <button className="block w-full mt-6 bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer">
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
