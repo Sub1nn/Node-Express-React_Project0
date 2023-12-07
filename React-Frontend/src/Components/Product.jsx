6c28be2d3fdaa13b36b1e8e0633d59db0abb0ef8
Subin <sub_1n@yahoo.com>
2023-12-07 17:25:30 +0200

    Product description added along with css

--- React-Frontend/src/Components/Product.jsx
+++ React-Frontend/src/Components/Product.jsx
@@ -1,7 +1,30 @@
-// eslint-disable-next-line react/prop-types
+import { Link } from "react-router-dom";
+
 const Product = ({ product }) => {
-  // eslint-disable-next-line react/prop-types
-  return <div>{`${product.f_name} ${product.l_name}`}</div>;
+  return (
+    <div className="bg-white rounded shadow-lg overflow-hidden">
+      <img src={product.Image} className="w-full h-30 object-cover" />
+      <div className="px-4 pb-4">
+        <h2 className="text font-semibold">{product.Name}</h2>
+        <div className="text-sm">Quantity: {product.Quantity}</div>
+        <div className="text-sm">Price ${product.Price}</div>
+        <div className="mt-2 flex gap-4">
+          <Link
+            to={`/edit`}
+            className="inline-block w-full text-center shadow-md text-sm bg-gray-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-gray-600 hover:cursor-pointer"
+          >
+            Edit
+          </Link>
+          <Link
+            to={`/edit`}
+            className="inline-block w-full text-center shadow-md text-sm bg-red-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-red-600 hover:cursor-pointer"
+          >
+            Delete
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
